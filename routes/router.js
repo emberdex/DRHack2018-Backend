0be8deca961fce6782160019e81c5df95768a3d2
Toby Jones <psytj1@exmail.nottingham.ac.uk>
2018-04-28 02:09:54 +0100

    api works kinda

---
+++ routes/router.js
@@ -0,0 +1,70 @@
+var config;
+
+try {
+	config = require("./../config/config.json");	
+} catch (ex) {
+	console.log("[Router] failed to import config.json.");
+	console.log("[Router] Please check that the file exists, and try again.")
+	process.exit(1);
+}
+
+const MongoClient = require('mongodb').MongoClient;
+
+exports.getRankings = (req, res) => {
+	// TODO: Get rankings for countryID and fieldName.
+}
+
+exports.getAll = (req, res) => {
+	(async function(req, res) {
+		let client;
+
+		try {
+			client = await MongoClient.connect(config.mongodb.connectionString);
+
+			console.log(`[Router] connected to MongoDB server ${config.mongodb.connectionString}`);
+			console.log(`[Router] getting all countries, oh man i am not good with database plz to help`);
+
+			const db = client.db('drhack2018');
+			const collection = db.collection('countries');
+
+			const result = await collection.find({year: 2012}).toArray();
+
+			res.status(200).json({status: "success", data: result});
+
+			client.close();
+		} catch (ex) {
+			console.log(ex.stack);
+		}
+	})(req, res);
+}
+
+exports.search = (req, res) => {
+	(async function(req, res) {
+		let client;
+
+		try {
+			client = await MongoClient.connect(config.mongodb.connectionString);
+
+			console.log(`[Router] connected to MongoDB server ${config.mongodb.connectionString}`);
+			console.log(`[Router] searching for country \"${req.params.countryName}\"`);
+
+			const db = client.db('drhack2018');
+			const collection = db.collection('countries');
+
+			const result = await collection.find({year: 2012}).toArray();
+
+			let results = [];
+			for(var i = 0; i < result.length; i++) {
+				if(result[i].state.includes(req.params.countryName)) {
+					results.push(result[i]);
+				}
+			}
+
+			res.status(200).json({status: "success", data: results});
+
+			client.close();
+		} catch (ex) {
+			console.log(ex.stack);
+		}
+	})(req, res);
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+const router = require('./routes/router.js');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bp = require('body-parser');
+
+var config;
+
+try {
+	config = require("./config/config.json");	
+} catch (ex) {
+	console.log("[Server-Main] failed to import config.json.");
+	console.log("[Server-Main] Please check that the file exists, and try again.");
+	process.exit(1);
+}
+
+const app = express();
+
+app.use(bp.json());
+
+app.get('/api/search/:countryName', router.search);
+app.get('/api/get_all', router.getAll);
+app.get('/api/get_rankings/:countryID/:fieldName', router.getRankings);
+
+app.listen(config.express.port, () => {
+	console.log(`[Server-Main] API listening on TCP port ${config.express.port}.`);
+})
